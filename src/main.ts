659e87120e99ea82e6c8874a3ba6b08672280661
Pan Nic <pannic1984@outlook.com>
2022-04-14 19:46:21 +0800

    5.1.4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router"
+import VConsole from 'vconsole'
 
-createApp(App).use(router).mount('#app')
+const vConsole = new VConsole();
+
+createApp(App).use(vConsole).use(router).mount('#app')
